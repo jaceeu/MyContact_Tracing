bce1e2c543a8d2995cfa4f9e7d660f8e7cd9648c
Jhan Carlo Vasquez <jhancarlo.vasquez@tup.edu.ph>
2022-06-06 11:02:07 +0800

    add background color

--- Contact Tracing/Form1.Designer.cs
+++ Contact Tracing/Form1.Designer.cs
@@ -69,6 +69,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCoral;
             this.ClientSize = new System.Drawing.Size(404, 341);
             this.Controls.Add(this.txtBox3);
             this.Controls.Add(this.txtBox2);
