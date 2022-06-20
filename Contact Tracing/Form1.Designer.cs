018d6c38c191ebcdd02ab48a004c4e8a1b1fc4bb
Jhan Carlo Vasquez <jhancarlo.vasquez@tup.edu.ph>
2022-06-20 13:45:13 +0800

    adjust

--- Contact Tracing/Form1.Designer.cs
+++ Contact Tracing/Form1.Designer.cs
@@ -70,7 +70,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightCoral;
-            this.ClientSize = new System.Drawing.Size(404, 341);
+            this.ClientSize = new System.Drawing.Size(415, 341);
             this.Controls.Add(this.txtBox3);
             this.Controls.Add(this.txtBox2);
             this.Controls.Add(this.txtBox1);
